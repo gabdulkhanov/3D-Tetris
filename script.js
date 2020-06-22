1d523ea737ca244b0101930a6a20798a8caae733
gabdulkhanov <gabdulkhanov.lenar@mail.ru>
2020-06-22 14:22:22 +0500

    rotation adjustment and field creation

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 window.onload = function() {
 	
 		var width = window.innerWidth-50;
-		var height = window.innerHeight-50;
-	
+		var height = window.innerHeight-50;	
 	
 		var scene = new THREE.Scene();
 		var camera = new THREE.PerspectiveCamera( 45, width / height, 0.1, 5000 );		
@@ -17,11 +16,11 @@ window.onload = function() {
 		
 		var controls = new THREE.OrbitControls( camera, renderer.domElement );
 		controls.target.set( 0, 0, 0 );
-		camera.position.set( -5, 10, 5 );		
+		camera.position.set( 0, 14, 0 );		
 		controls.update();
 		
 		
-		
+		/****LIGHT****/
 		{
 			var light = new THREE.DirectionalLight(0xffffff, 1);
 			light.position.set(-1, 2, 4);
@@ -32,17 +31,155 @@ window.onload = function() {
 			scene.add(light2);
 		}
 
-		var geometry = new THREE.BoxGeometry(1,1,1);
-		var material = new THREE.MeshPhongMaterial( { color: 0xffffff} );
+		/****GEOMETRY AND MATERIAL******/
+		var geometry = new THREE.BoxGeometry(1 ,1 ,1);
+		var material = new THREE.MeshPhongMaterial( { color: 0xffffff} );	
+		var material_center_figure = new THREE.MeshPhongMaterial( { color: 0xff0000} );	
+
+		var material_field = new THREE.MeshPhongMaterial( { color: 0x00ff00 } );		
+		
+		
+		/***РЁБРА КУБА***/
+		var geo_line = new THREE.EdgesGeometry( geometry );
+		var mat_line = new THREE.LineBasicMaterial( { color: 0x000000, linewidth: 4} );	
+		
+
+		/***ARRAYS FIELD AND FIGURE***/
+		var field = [	
+						[
+							[1,1,1,1,1,1,1,1], /***0***/
+							[1,1,1,1,1,1,1,1],
+							[1,1,1,1,1,1,1,1],
+							[1,1,1,1,1,1,1,1],
+							[1,1,1,1,1,1,1,1],
+							[1,1,1,1,1,1,1,1],
+							[1,1,1,1,1,1,1,1],
+							[1,1,1,1,1,1,1,1]
+						],
+						
+						[
+							[1,1,1,1,1,1,1,1], /***1***/
+							[1,0,0,0,0,0,0,1],
+							[1,0,0,0,0,0,0,1],
+							[1,0,0,0,0,0,0,1],
+							[1,0,0,0,0,0,0,1],
+							[1,0,0,0,0,0,0,1],
+							[1,0,0,0,0,0,0,1],
+							[1,1,1,1,1,1,1,1]
+						],
+						
+						[
+							[1,1,1,1,1,1,1,1], /***2***/
+							[1,0,0,0,0,0,0,1],
+							[1,0,0,0,0,0,0,1],
+							[1,0,0,0,0,0,0,1],
+							[1,0,0,0,0,0,0,1],
+							[1,0,0,0,0,0,0,1],
+							[1,0,0,0,0,0,0,1],
+							[1,1,1,1,1,1,1,1]
+						],
+						
+						[
+							[1,1,1,1,1,1,1,1], /***3***/
+							[1,0,0,0,0,0,0,1],
+							[1,0,0,0,0,0,0,1],
+							[1,0,0,0,0,0,0,1],
+							[1,0,0,0,0,0,0,1],
+							[1,0,0,0,0,0,0,1],
+							[1,0,0,0,0,0,0,1],
+							[1,1,1,1,1,1,1,1]
+						],
+						
+						[
+							[1,1,1,1,1,1,1,1], /***4***/
+							[1,0,0,0,0,0,0,1],
+							[1,0,0,0,0,0,0,1],
+							[1,0,0,0,0,0,0,1],
+							[1,0,0,0,0,0,0,1],
+							[1,0,0,0,0,0,0,1],
+							[1,0,0,0,0,0,0,1],
+							[1,1,1,1,1,1,1,1]
+						],
+						
+						[
+							[1,1,1,1,1,1,1,1], /***5***/
+							[1,0,0,0,0,0,0,1],
+							[1,0,0,0,0,0,0,1],
+							[1,0,0,0,0,0,0,1],
+							[1,0,0,0,0,0,0,1],
+							[1,0,0,0,0,0,0,1],
+							[1,0,0,0,0,0,0,1],
+							[1,1,1,1,1,1,1,1]
+						],
+						
+						[
+							[1,1,1,1,1,1,1,1], /***6***/
+							[1,0,0,0,0,0,0,1],
+							[1,0,0,0,0,0,0,1],
+							[1,0,0,0,0,0,0,1],
+							[1,0,0,0,0,0,0,1],
+							[1,0,0,0,0,0,0,1],
+							[1,0,0,0,0,0,0,1],
+							[1,1,1,1,1,1,1,1]
+						],
+						
+						[
+							[1,1,1,1,1,1,1,1], /***7***/
+							[1,0,0,0,0,0,0,1],
+							[1,0,0,0,0,0,0,1],
+							[1,0,0,0,0,0,0,1],
+							[1,0,0,0,0,0,0,1],
+							[1,0,0,0,0,0,0,1],
+							[1,0,0,0,0,0,0,1],
+							[1,1,1,1,1,1,1,1]
+						],
+						
+						[
+							[1,1,1,1,1,1,1,1], /***8***/
+							[1,0,0,0,0,0,0,1],
+							[1,0,0,0,0,0,0,1],
+							[1,0,0,0,0,0,0,1],
+							[1,0,0,0,0,0,0,1],
+							[1,0,0,0,0,0,0,1],
+							[1,0,0,0,0,0,0,1],
+							[1,1,1,1,1,1,1,1]
+						],
+						
+						[
+							[1,1,1,1,1,1,1,1], /***9***/
+							[1,0,0,0,0,0,0,1],
+							[1,0,0,0,0,0,0,1],
+							[1,0,0,0,0,0,0,1],
+							[1,0,0,0,0,0,0,1],
+							[1,0,0,0,0,0,0,1],
+							[1,0,0,0,0,0,0,1],
+							[1,1,1,1,1,1,1,1]
+						],
+						
+						[
+							[1,1,1,1,1,1,1,1], /***10***/
+							[1,0,0,0,0,0,0,1],
+							[1,0,0,0,0,0,0,1],
+							[1,0,0,0,0,0,0,1],
+							[1,0,0,0,0,0,0,1],
+							[1,0,0,0,0,0,0,1],
+							[1,0,0,0,0,0,0,1],
+							[1,1,1,1,1,1,1,1]
+						],					
+						
+					];
+
+
+
 
 		var figure = [
 						 [[0,0,0],
 						  [0,0,0],
-						  [0,0,0]],
+						  [0,1,0]],
 						
-						 [[0,0,0],
-						  [0,1,1],
-						  [1,1,0]],
+						 [[0,1,1],
+						  [0,2,0],
+						  [0,1,0]],
 						  
 						 [[0,0,0],
 						  [0,0,0],
@@ -50,65 +187,50 @@ window.onload = function() {
 						
 					];
 					
-				
-		let flag = true;
-	
-	
 		
-		addScene();
-		
-	
-		
-		renderer.render( scene, camera );
+		/*******************/
 		
 		
+		addScene_field();		
+		addScene();			
+		renderer.render( scene, camera );		
 		
 		
+		/***********Управление*************/
 		window.onkeydown = function(e) {
-			if (e.keyCode === 37) {				
-			
+			if (e.keyCode === 65) {	/***A***/			
 				clearScene();				
 				figure = rotate90_x(figure, true);
-									
-				addScene();
-				
-			};
-			if (e.keyCode === 38) {			
-				
-				clearScene();			
-				figure = rotate90_y(figure, flag, false);				
+				addScene_field();					
 				addScene();				
-				flag = !flag;
-				
+			};			
+			if (e.keyCode === 87) {	/***W***/			
+				clearScene();			
+				figure = rotate90_y(figure, false);	
+				addScene_field();
+				addScene();					
 			};
-			if (e.keyCode === 39) {
-				
+			if (e.keyCode === 68) { /***D***/				
 				clearScene();				
 				figure = rotate90_x(figure, false);	
-			
-				addScene();
-				
-			};
-			if (e.keyCode === 40) {
-				
-				clearScene();				
-				figure = rotate90_y(figure, flag, true);			
+				addScene_field();
 				addScene();				
-				flag = !flag;
-			
-			};
-			
-			
-			renderer.render( scene, camera );
-			
+			};			
+			if (e.keyCode === 83) { /***S***/				
+				clearScene();				
+				figure = rotate90_y(figure, true);	
+				addScene_field();
+				addScene();				
+			};				
+			renderer.render( scene, camera );			
 		}
 		
 		
-		window.onmousedown = animate;	
-		
+		window.onmousedown = animate;		
 		window.onresize = resize;
 		
 		
+		/*********Функции**************/
 		
 		function resize() {			
 			renderer.setSize(width, height);
@@ -119,7 +241,8 @@ window.onload = function() {
 		function animate() {
 
 			requestAnimationFrame( animate );	
-			controls.target.set( 0, 0, 0 );			
+			controls.target.set( 0, 0, 0 );		
+			camera.position.set( 0, 14, 0 );//убрать строку для вращения камеры
 			controls.update();
 			renderer.render( scene, camera );
 
@@ -130,24 +253,51 @@ window.onload = function() {
 			for (let i = 0; i < figure.length; ++i)
 				for (let j = 0; j < figure[i].length; ++j)
 					for (let k = 0; k < figure[i][j].length; ++k)					
-					{	
+					{
 						if (figure[i][j][k]) {
-							var cube = new THREE.Mesh( geometry, material );
+							let wireframe = new THREE.LineSegments( geo_line, mat_line );
+							wireframe.position.set( k, i, j );
+							scene.add( wireframe );
+						}
+						if (figure[i][j][k] === 1) {
+							let cube = new THREE.Mesh( geometry, material );
 							cube.position.set( k, i, j );							
 							scene.add(cube);
-						}						
+							
+						}	
+						else if (figure[i][j][k] === 2){
+							let cube = new THREE.Mesh( geometry, material_center_figure );
+							cube.position.set( k, i, j );							
+							scene.add(cube);
+							
+						}
 					}
 		}
 		
+		
+		function addScene_field() {
+			for (let i = 0; i < field.length; ++i)
+				for (let j = 0; j < field[i].length; ++j)
+					for (let k = 0; k < field[i][j].length; ++k)					
+					{	
+						if (field[i][j][k]) {
+							let cube_field = new THREE.Mesh( geometry, material_field );
+							cube_field.position.set( k-3, i-9, j-3 );							
+							scene.add(cube_field);
+							let wireframe = new THREE.LineSegments( geo_line, mat_line );
+							wireframe.position.set( k-3, i-9, j-3);
+							scene.add( wireframe );
+						}							
+					}
+		}	
+
+
 		function clearScene() {
 			for (let i = scene.children.length - 1; i >= 0; i--) {
-				if(scene.children[i].type === "Mesh")
+				if ((scene.children[i].type === "Mesh")||(scene.children[i].type === "LineSegments"))
 					scene.remove(scene.children[i]);
 			}
-		}
-		
-		
-		
+		}		
 		
 		
 		function rotate90_x(mas, reverse) {
@@ -166,39 +316,20 @@ window.onload = function() {
 				}
 			}
 			return new_mas;
-		}	
-
-
-	
+		}		
 		
 		
 		
-		function rotate90_y(mas, flag, reverse) {
-			var new_mas = new_array3x3(mas);
-			
-			if (flag) {
-				for (let i = 0; i < mas.length; i++) 
-					for (let m = 0; m < mas[i].length; m++)
-						for (let n = 0; n < mas[i][m].length; n++) {
-							if (reverse) new_mas[mas.length-i-1][n][m] = mas[n][i][m];	
-							else new_mas[i][n][m] = mas[n][i][m];								
-						}
-			}
-			else {
-				for (let i = 0; i < mas.length; i++) 
-					for (let m = 0; m < mas[i].length; m++) 
-						for (let n = 0; n < mas[i][m].length; n++) {
-							if (reverse) new_mas[n][i][m] = mas[i][n][m];
-							else new_mas[n][mas.length-i-1][m] = mas[i][n][m];								
-					}
-			}
-			
+		function rotate90_y(mas, reverse) {
+			var new_mas = new_array3x3(mas);			
+			for (let i = 0; i < mas.length; i++) 
+				for (let m = 0; m < mas[i].length; m++)
+					for (let n = 0; n < mas[i][m].length; n++) {
+						if (reverse) new_mas[mas.length-i-1][n][m] = mas[n][i][m];	
+						else new_mas[i][mas.length-n-1][m] = mas[n][i][m];								
+					}		
 			return new_mas;
-		}	
-		
-		
-		
-			
+		}			
 		
 		function new_array3x3(mas) {
 			var new_mas = [];
@@ -214,8 +345,7 @@ window.onload = function() {
 				}
 			}
 			return new_mas;
-		}
-		
+		}		
 }
 
 
